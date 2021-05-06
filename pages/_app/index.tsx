710b37a00a9a17cc535ce3b382f6b6725367fdd0
Umma Ahimsha <uahimsha@gmail.com>
2021-05-07 01:43:33 +0700

    added NProgress loading indicator

--- pages/_app/index.tsx
+++ pages/_app/index.tsx
@@ -2,8 +2,10 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
 
 import Layout from 'components/Layout';
+import NProgress from 'components/NProgress';
 
 import 'styles/main.css';
+import 'styles/nprogress.css';
 import GlobalStyles from 'styles/globals';
 
 export default function MyApp({ Component, pageProps, router }: AppProps) {
@@ -14,6 +16,7 @@ export default function MyApp({ Component, pageProps, router }: AppProps) {
         <Layout>
           <Component {...pageProps} key={router.route} />
         </Layout>
+        <NProgress />
       </ThemeProvider>
     </>
   );
